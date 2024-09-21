0db5dd5bb6bb71347cf452ad4dad0a77c5a1a283
rodrigo <lacan.xrl80@gmail.com>
2024-09-21 18:07:20 -0400

    Modularizando lógica e responsabilidades no create command

--- src/cmd/create.go
+++ src/cmd/create.go
@@ -1,19 +1,38 @@
 package cmd
 
-import(
-	"github.com/spf13/cobra"
+import (
 	"fmt"
+	"github.com/spf13/cobra"
+	"SMC/src/cmd/createcommands" // Ajuste para o caminho correto
 )
 
-var initCmd1 = &cobra.Command{
-	Use:   "init-i",
-	Short: "Baixa um projeto Spring Boot da API pública",
+var (
+	repositoryName string
+	controllerName string
+	serviceName    string
+)
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Cria recursos para a aplicação Spring",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Executando o comando init-i...")
-		// Adicione aqui a lógica para o comando init-i, se necessário
+		switch {
+		case repositoryName != "":
+			createcommands.CreateRepository(repositoryName) // Chama a função de criar repositório
+		case controllerName != "":
+			createcommands.CreateController(controllerName) // Chama a função de criar controlador
+		case serviceName != "":
+			createcommands.CreateService(serviceName) // Chama a função de criar serviço
+		default:
+			fmt.Println("Por favor, forneça um nome para repository, controller ou service.")
+		}
 	},
 }
 
-func init(){
-	rootCmd.AddCommand(initCmd1)
+func init() {
+	createCmd.Flags().StringVarP(&repositoryName, "repository", "r", "", "Cria um repositório com o nome especificado")
+	createCmd.Flags().StringVarP(&controllerName, "controller", "l", "", "Cria um controlador com o nome especificado")
+	createCmd.Flags().StringVarP(&serviceName, "service", "s", "", "Cria um serviço com o nome especificado")
+
+	RootCmd.AddCommand(createCmd)
 }
