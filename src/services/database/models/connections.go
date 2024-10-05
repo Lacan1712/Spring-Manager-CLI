32006347d94dd0ea3e74eea9ac796064ac700f56
rodrigo <lacan.xrl80@gmail.com>
2024-10-05 14:46:08 -0400

    Adicionando novo campo ao struct

--- src/services/database/models/connections.go
+++ src/services/database/models/connections.go
@@ -1,13 +1,14 @@
 package models
 
 type Connections struct {
-	DriveDatabase string `json:"driveDatabase"` 
-    DatabaseName  string `json:"databaseName"`
-    Host   		  string `json:"host"`
-	Port   		  string `json:"port"`
-	Username 	  string `json:"username"`
-	Password 	  string `json:"password"`
-	Schema		  string `json:"schema"`
+	ConnectionName string `json:"connectionName"`
+	DriveDatabase  string `json:"driveDatabase"` 
+    DatabaseName   string `json:"databaseName"`
+    Host   		   string `json:"host"`
+	Port   		   string `json:"port"`
+	Username 	   string `json:"username"`
+	Password 	   string `json:"password"`
+	Schema		   string `json:"schema"`
 }
 
 type Database struct {
