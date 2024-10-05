1d6645545220b4a95731318a9a3e0f2a834409bc
rodrigo <lacan.xrl80@gmail.com>
2024-10-04 21:56:47 -0400

    Criando comando database

---
+++ src/cmd/database.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"smc/src/cmd/databasecommands"
+)
+
+var (
+	listTables bool
+)
+
+var databaseCmd = &cobra.Command{
+	Use:   "database",
+	Short: "Recursos para manipulação de banco de dados",
+	Run: func(cmd *cobra.Command, args []string) {
+			if(listTables){
+				databasecommands.ListTables()
+			}
+	},
+}
+
+func init() {
+	databaseCmd.Flags().BoolVarP(&listTables, "listables", "",false,"lista as tabelas do banco")
+
+	rootCmd.AddCommand(databaseCmd)
+}
