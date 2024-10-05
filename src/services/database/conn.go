9677bbec6ca8e635331b6a12e6e94d7dfae2ce50
rodrigo <lacan.xrl80@gmail.com>
2024-10-05 14:45:21 -0400

    Criando função de listar tabelas no service de databases

--- src/services/database/conn.go
+++ src/services/database/conn.go
@@ -2,6 +2,8 @@ package database
 
 import(
 	"fmt"
+    "log"
+    jsonservice "smc/src/services/json"
 	_ "github.com/go-sql-driver/mysql"
     _ "github.com/lib/pq"               
     _ "github.com/denisenkom/go-mssqldb"
@@ -35,3 +37,64 @@ func ConnectToDatabase(conn models.Connections) (*sql.DB, error) {
     return db, nil
 }
 
+
+func ListTablesDB(connectionName string) {
+    var database models.Database
+
+    err := jsonservice.MappingStructToJson("/home/rodrigo/Documentos Local/Projetos/Go/SMC/src/cmd/databasecommands/database.json", &database)
+    if err != nil {
+        fmt.Printf("Error reading JSON: %v", err)
+		return
+    }
+
+
+    // Encontrar a conexão correta com base no connectionName
+    var conn *models.Connections
+    for _, connection := range database.Connections {
+        if connection.ConnectionName == connectionName {
+            conn = &connection
+            break
+        }
+    }
+
+    if conn == nil {
+        fmt.Printf("Conexão com o nome '%s' não foi encontrada.\n", connectionName)
+		return
+	}
+
+	db, err := ConnectToDatabase(*conn) 
+        if err != nil {
+            fmt.Printf("Failed to connect to %s: %v\n", conn.DatabaseName, err)
+			return
+        }
+
+        fmt.Printf("Connected to database: %s\n", conn.DatabaseName)
+
+        schema := "public"
+        if conn.Schema != "" {
+            schema = conn.Schema
+        }
+
+        query := fmt.Sprintf(`SELECT table_name FROM information_schema.tables WHERE table_schema = '%s'`, schema)
+
+        rows, err := db.Query(query)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer rows.Close()
+
+        fmt.Println("Tabelas encontradas:")
+        for rows.Next() {
+            var tableName string
+            if err := rows.Scan(&tableName); err != nil {
+                log.Fatal(err)
+            }
+            fmt.Println(tableName)
+        }
+
+        if err = rows.Err(); err != nil {
+            log.Fatal(err)
+        }
+        db.Close()
+    
+}
